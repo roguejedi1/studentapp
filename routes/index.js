a37c71ac9fbba14d5107b97bd38fb6bec675ac0c
Rogue Jedi <kkvsith@gmail.com>
2020-08-21 00:44:05 +0100

    add index routes

---
+++ routes/index.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/about', (req, res, next) => {
+  res.render('about');
+});
+
+router.get('/login', (req, res, next) => {
+  res.render('login');
+});
+
+router.get('/register', (req, res, next) => {
+  res.render('register');
+});
+
+module.exports = router;
